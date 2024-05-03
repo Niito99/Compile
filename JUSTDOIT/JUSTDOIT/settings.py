9bfa3512d9fa1736effe06acf1a7c7bd317e0805
Daniel Okine <therealokine1@gmail.com>
2024-05-03 22:14:25 +0100

    Templates Config

--- JUSTDOIT/JUSTDOIT/settings.py
+++ JUSTDOIT/JUSTDOIT/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'JUSTDOIT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["BASE_DIR/Templates", "BASE_DIR/base"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
